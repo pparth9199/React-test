88563ceb0177a29a721f553176b7e43ba04aaf8f
Parth Patel <parthjpatel9199@gmail.com>
2022-04-08 02:26:49 -0700

    commit message

--- src/App.js
+++ src/App.js
@@ -11,4 +11,14 @@ function App(){
 );
 }
 
-export default App;
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (_props) => (
+  <Scene style={{ width: '70vw', height: '90vh' }} 
+      mapProperties={{ basemap: 'satellite' }}
+      viewProperties={{
+          center: [-118.28538,34.0205],
+          zoom: 15
+      }}>
+      <Campus />
+  </Scene>
+)
